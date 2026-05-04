cfcbe48c5c52f306e7c16534e49c766173055cf7
Anantha <atoleanantha03@gmail.com>
2026-05-04 09:16:49 +0530

    redux setup with authReducer example

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+// confing store
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlices';
+const store = configureStore({
+    reducer: {
+        auth: authReducer,
+    },
+});
+
+export default store;
