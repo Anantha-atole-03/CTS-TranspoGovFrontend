8ddabf81139f5ea0553a702127b3a49c96dfe2c3
Adarsh3313 <adarshreddymiddi@gmail.com>
2026-05-05 15:17:37 +0530

    Create RouteScheduleDashboard component

---
+++ src/pages/features/routes/RouteScheduleDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RouteScheduleDashboard = () => {
+    return (
+        <div>
+            <h1>Route Schedule Dashboard</h1>
+        </div>
+    );
+};
+
+export default RouteScheduleDashboard;
